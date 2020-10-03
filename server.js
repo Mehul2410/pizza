a389b79f463e2626e1c62d80f100dee11d8861bd
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2020-10-02 17:23:56 -0700

    login and register routes and auth

--- server.js
+++ server.js
@@ -6,14 +6,24 @@ const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static("public"));
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
 // set template engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/cart", (req, res) => {
+  res.render("customers/cart");
+});
+app.get("/login", (req, res) => {
+  res.render("auth/login");
+});
+app.get("/register", (req, res) => {
+  res.render("auth/register");
+});
 
 app.listen(PORT, () => {
   console.log(`hello ${PORT}`);
