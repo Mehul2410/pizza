a0e93de146c8154f33cca2b7171f09b6073c9ef4
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2020-10-04 11:18:53 -0700

    routes nodejs homepage data fetch using mongodb

--- server.js
+++ server.js
@@ -1,9 +1,42 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const ejs = require("ejs");
 const path = require("path");
 const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("express-flash");
+
+// database CONNECTION
+const url = "mongodb+srv://Pizza:m1e2h3u4l5@pizza.l8rmk.mongodb.net/pizzaD";
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+  useFindAndModify: true,
+});
+const connection = mongoose.connection;
+connection
+  .once("open", () => {
+    console.log("databse connected...");
+  })
+  .catch((err) => {
+    console.log("connection failed...");
+  });
+
+//session configuration store:mongoStore,
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+  })
+);
+
+app.use(flash());
 
 app.use(express.static("public"));
 // set template engine
@@ -11,19 +44,7 @@ app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/cart", (req, res) => {
-  res.render("customers/cart");
-});
-app.get("/login", (req, res) => {
-  res.render("auth/login");
-});
-app.get("/register", (req, res) => {
-  res.render("auth/register");
-});
+require("./routes/web")(app);
 
 app.listen(PORT, () => {
   console.log(`hello ${PORT}`);
