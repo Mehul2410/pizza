6c2864e5d1fd6d7a17fa8e12110f05c2dbd981d7
Mehul2410 <mehulgawhale18hc@student.mes.ac.in>
2020-10-09 10:01:25 -0700

    singleOrder status

--- routes/web.js
+++ routes/web.js
@@ -2,7 +2,8 @@ const homeController = require("../app/http/controllers/homeController");
 const authController = require("../app/http/controllers/authController");
 const cartController = require("../app/http/controllers/customers/cartController");
 const orderController = require("../app/http/controllers/customers/orderController");
-const AdminOrderController = require("../app/http/controllers/admin/orderController");
+const adminOrderController = require("../app/http/controllers/admin/orderController");
+const statusController = require("../app/http/controllers/admin/statusController");
 //middle wares
 const guest = require("../app/http/middleware/guest");
 const auth = require("../app/http/middleware/auth");
@@ -25,10 +26,15 @@ function initRoutes(app) {
 
   // customer routes
   app.post("/orders", auth, orderController().store);
-  app.get("/customer/orders", auth, orderController().index);
+  app.get("/customer/orders", auth, orderController().index);  app.get("/customer/orders", auth, orderController().index);
+  app.get("/customer/orders/:id", auth, orderController().show);
+
 
   //Admin routes
-  app.get("/admin/orders", admin, AdminOrderController().index);
+  app.get("/admin/orders", admin, adminOrderController().index);
+  //admin order status
+  app.post("/admin/order/status", admin, statusController().update);
+
 }
 
 module.exports = initRoutes;
